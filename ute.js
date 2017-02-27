df28a17f801172ae3614c13bfe8184f3f2e4e23b
bonford <bonford@users.noreply.github.com>
2017-02-26 22:42:27 -0500

    initial phantomjs scrape setup

---
+++ ute.js
@@ -0,0 +1,16 @@
+var page = require('webpage').create();
+page.open('http://finance.yahoo.com/stock-center/?bypass=true', function(status) {
+  console.log("Status: " + status);
+  if(status === "success") {
+	  var doc = page.evaluate(function() {
+		 
+    return document;
+	 
+  });
+   // page.render('example.png');
+ 
+  }
+  console.log(doc.title);
+   
+  phantom.exit();
+});
