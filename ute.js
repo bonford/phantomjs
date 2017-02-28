9c3beae56f84926b77b23517bd2131a1924a3e48
bonford <bonford@users.noreply.github.com>
2017-02-28 10:31:12 -0500

    added a notes file and a json.task file

--- ute.js
+++ ute.js
@@ -1,4 +1,11 @@
 var page = require('webpage').create();
+page.onResourceRequested = function (req) {
+    console.log('requested: ' + JSON.stringify(req, undefined, 4));
+};
+
+page.onResourceReceived = function (res) {
+    console.log('received: ' + JSON.stringify(res, undefined, 4));
+};
 page.open('http://finance.yahoo.com/stock-center/?bypass=true', function(status) {
   console.log("Status: " + status);
   if(status === "success") {
